eecbb731399bb5c040c3f66247df7958e4b75ef6
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-03-03 22:29:43 -0500

    wip sync

--- sync/sync_test.go
+++ sync/sync_test.go
@@ -9,8 +9,13 @@ func TestCounter(t *testing.T) {
 		counter.Inc()
 		counter.Inc()
 
-		if counter.Value() != 3 {
-			t.Errorf("got %d, want %d", counter.Value(), 3)
-		}
+		assertCounter(t, counter, 3)
 	})
+}
+
+func assertCounter(t testing.TB, got Counter, want int) {
+	t.Helper()
+	if got.value != want {
+		t.Errorf("got %d, want %d", got.value, want)
+	}
 }
