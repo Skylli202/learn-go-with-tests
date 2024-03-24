233ab46bb95b37126322475b9e31807d4c0ca221
egouinguenet <egouinguenet@dalet.com>
2024-03-24 14:48:51 -0400

    wip clockface

--- math/clockface/clockface.go
+++ math/clockface/clockface.go
@@ -5,25 +5,9 @@ import (
 	"time"
 )
 
-const (
-	secondHandLength = 90
-	clockCentreX     = 150
-	clockCentreY     = 150
-)
-
 type Point struct {
 	X float64
 	Y float64
-}
-
-func SecondHand(t time.Time) Point {
-	p := secondHandPoint(t)
-
-	p = Point{p.X * secondHandLength, p.Y * secondHandLength} // scale
-	p = Point{p.X, -p.Y}                                      // flip
-	p = Point{p.X + clockCentreX, p.Y + clockCentreY}         // translate
-
-	return p
 }
 
 func secondsInRadians(t time.Time) float64 {
--- math/clockface/clockface_acceptance_test.go
+++ math/clockface/clockface_acceptance_test.go
@@ -1,30 +1,54 @@
 package clockface_test
 
 import (
+	"bytes"
+	"encoding/xml"
 	"testing"
 	"time"
 
 	"github.com/Skylli202/learn-go-with-tests/math/clockface"
 )
 
-func TestSecondHandAtMidnight(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
-
-	want := clockface.Point{X: 150, Y: 150 - 90}
-	got := clockface.SecondHand(tm)
-
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
-	}
+type Svg struct {
+	XMLName xml.Name `xml:"svg"`
+	Text    string   `xml:",chardata"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  struct {
+		Text  string `xml:",chardata"`
+		Cx    string `xml:"cx,attr"`
+		Cy    string `xml:"cy,attr"`
+		R     string `xml:"r,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"circle"`
+	Line []struct {
+		Text  string `xml:",chardata"`
+		X1    string `xml:"x1,attr"`
+		Y1    string `xml:"y1,attr"`
+		X2    string `xml:"x2,attr"`
+		Y2    string `xml:"y2,attr"`
+		Style string `xml:"style,attr"`
+	} `xml:"line"`
 }
 
-func TestSecondHandAt30Seconds(t *testing.T) {
-	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+func TestSVGWriterAtMidnight(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 0, 0, time.UTC)
 
-	want := clockface.Point{X: 150, Y: 150 + 90}
-	got := clockface.SecondHand(tm)
+	b := bytes.Buffer{}
+	clockface.SVGWriter(&b, tm)
 
-	if got != want {
-		t.Errorf("Got %v, wanted %v", got, want)
+	svg := Svg{}
+	xml.Unmarshal(b.Bytes(), &svg)
+
+	X2 := "150"
+	Y2 := "60"
+
+	for _, line := range svg.Line {
+		if line.X2 == X2 && line.Y2 == Y2 {
+			return
+		}
 	}
 }
