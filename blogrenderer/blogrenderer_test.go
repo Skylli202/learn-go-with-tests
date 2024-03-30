fe7893f80fea6eadd6d3a6e834e2fefc7b4da9f2
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-03-29 23:24:14 -0700

    wip blogrenderer

--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -2,9 +2,11 @@ package blogrenderer_test
 
 import (
 	"bytes"
+	"io"
 	"testing"
 
 	"github.com/Skylli202/learn-go-with-tests/blogrenderer"
+	approvals "github.com/approvals/go-approval-tests"
 )
 
 func TestRender(t *testing.T) {
@@ -15,18 +17,37 @@ func TestRender(t *testing.T) {
 		Tags:        []string{"go", "tdd"},
 	}
 
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := blogrenderer.Render(&buf, aPost)
-		if err != nil {
+
+		if err := postRenderer.Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
-		got := buf.String()
-		want := `<h1>hello world</h1>`
-
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		approvals.VerifyString(t, buf.String())
 	})
+}
+
+func BenchmarkRender(b *testing.B) {
+	aPost := blogrenderer.Post{
+		Title:       "hello world",
+		Body:        "This is a post",
+		Description: "This is a description",
+		Tags:        []string{"go", "tdd"},
+	}
+
+	postRenderer, err := blogrenderer.NewPostRenderer()
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		postRenderer.Render(io.Discard, aPost)
+	}
 }
--- blogrenderer/post.go
+++ blogrenderer/post.go
@@ -1,7 +1,8 @@
 package blogrenderer
 
 import (
-	"fmt"
+	"embed"
+	"html/template"
 	"io"
 )
 
@@ -10,7 +11,26 @@ type Post struct {
 	Tags                     []string
 }
 
-func Render(w io.Writer, p Post) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1>", p.Title)
-	return err
+type PostRenderer struct {
+	templ *template.Template
+}
+
+//go:embed "templates/*"
+var postTemplate embed.FS
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplate, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	return &PostRenderer{templ: templ}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	if err := r.templ.ExecuteTemplate(w, "blog.gohtml", p); err != nil {
+		return err
+	}
+
+	return nil
 }
