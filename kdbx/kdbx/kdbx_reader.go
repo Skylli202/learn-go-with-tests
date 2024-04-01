0648e0b97393dd10cb8a852f21e6d2a1d0cbe947
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-03-31 17:21:06 -0700

    kdbx: reading headers (wip)

--- kdbx/kdbx/kdbx_reader.go
+++ kdbx/kdbx/kdbx_reader.go
@@ -2,6 +2,7 @@ package kdbx
 
 import (
 	"encoding/binary"
+	"fmt"
 )
 
 // KDBX File Format Specification
@@ -9,6 +10,12 @@ import (
 
 type KDBX struct {
 	Data []byte
+}
+
+type Header struct {
+	Value  []byte
+	ID     uint8
+	Length uint32
 }
 
 func (kdbx *KDBX) ReadSignture1() uint32 {
@@ -24,4 +31,41 @@ func (kdbx *KDBX) FormatVersion() (uint32, uint32) {
 	minor := binary.LittleEndian.Uint16(kdbx.Data[8:10])
 
 	return uint32(major), uint32(minor)
+}
+
+func (kdbx *KDBX) ReadHeaders() []Header {
+	headers := make([]Header, 0)
+
+	var i uint32 = 12
+	id := uint8(kdbx.Data[i])
+	i++
+
+	for id != 0 {
+		header := Header{}
+		header.ID = id
+
+		switch header.ID {
+		case 0:
+			fmt.Println("TODO")
+		case 1, 5, 6, 8, 9, 10:
+			fmt.Println("Deprecated header found. Ignoring it.")
+		case 2, 3, 4, 7:
+			header.Length = binary.LittleEndian.Uint32(kdbx.Data[i:(i + 4)])
+			i += 4
+			header.Value = kdbx.Data[i:(i + header.Length)]
+			i += header.Length
+		default:
+			fmt.Println("TODO")
+		}
+
+		headers = append(headers, header)
+		if i < uint32(len(kdbx.Data)) {
+			id = uint8(kdbx.Data[i])
+			i += 1
+		} else {
+			id = 0
+		}
+	}
+
+	return headers
 }
--- kdbx/main.go
+++ kdbx/main.go
@@ -1,39 +1,38 @@
 package main
 
 import (
-	"encoding/binary"
 	"fmt"
+	"io/fs"
 	"os"
+
+	"github.com/Skylli202/learn-go-with-tests/kdbx/kdbx"
 )
 
 func main() {
-	fmt.Println("Hello there")
-
-	dat, err := os.ReadFile("./sample.kdbx")
-	check(err)
-
-	signature1 := binary.LittleEndian.Uint32(dat[:4])
-	fmt.Printf("Signature 1: 0x%02X\n", signature1)
-
-	signature2 := binary.LittleEndian.Uint32(dat[4:8])
-	fmt.Printf("Signature 2: 0x%02X\n", signature2)
-
-	version := binary.LittleEndian.Uint32(dat[8:12])
-	fmt.Printf("Version: 0x%02X\n", version)
-
-	fmt.Println("Headers:")
-	fmt.Printf("ID: 0x%02X\n", dat[12])
-	headerLength := binary.LittleEndian.Uint32(dat[13:17])
-	fmt.Printf("Length: 0x%02X (%d)\n", headerLength, headerLength)
-	fmt.Printf("Value: ")
-	for _, byte := range dat[17:(17 + 16)] {
-		fmt.Printf("%X", byte)
+	data, err := fs.ReadFile(os.DirFS("."), "sample.kdbx")
+	if err != nil {
+		panic(err)
 	}
-	fmt.Println("")
 
-	foo := []byte{0x00, 0x04}
-	foo = append(foo, 0x12)
-	fmt.Printf("%X %X %X\n", foo[0], foo[1], foo[2])
+	kdbx := kdbx.KDBX{
+		Data: data,
+	}
+
+	major, minor := kdbx.FormatVersion()
+
+	fmt.Printf(`Signature 1: %X
+Signature 2: %X
+Format version: %d.%d
+`, kdbx.ReadSignture1(), kdbx.ReadSignture2(), major, minor)
+
+	headers := kdbx.ReadHeaders()
+	for i, header := range headers {
+		fmt.Printf(`[%d]:
+  ID: %d,
+  Length: %d,
+  Value: %X
+`, i, header.ID, header.Length, header.Value)
+	}
 }
 
 func check(e error) {
