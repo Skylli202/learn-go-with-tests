15efa88fa59fbe7a7de8d575c8def1e677b5a75f
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-04-02 21:28:23 -0700

    revisiting arrays done

--- arrays/bad_bank.go
+++ arrays/bad_bank.go
@@ -6,17 +6,34 @@ type Transaction struct {
 	Sum  float64
 }
 
-func BalanceFor(transactions []Transaction, name string) float64 {
-	var balance float64
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
 
-	for _, t := range transactions {
-		if t.From == name {
-			balance = balance - t.Sum
-		}
-		if t.To == name {
-			balance = balance + t.Sum
-		}
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewAccount(name string, balance float64) Account {
+	return Account{Name: name, Balance: balance}
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if a.Name == transaction.From {
+		a.Balance = a.Balance - transaction.Sum
+	}
+	if a.Name == transaction.To {
+		a.Balance = a.Balance + transaction.Sum
 	}
 
-	return balance
+	return a
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -69,3 +69,15 @@ func TestReduce(t *testing.T) {
 		generics.AssertEqual(t, Reduce([]string{"a", "b", "c"}, concatenate, ""), "abc")
 	})
 }
+
+func TestFind(t *testing.T) {
+	t.Run("find first even number", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+
+		firstEvenNumber, found := Find(numbers, func(x int) bool {
+			return x%2 == 0
+		})
+		generics.AssertTrue(t, found)
+		generics.AssertEqual(t, firstEvenNumber, 2)
+	})
+}
