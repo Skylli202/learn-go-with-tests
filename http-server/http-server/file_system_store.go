5b6779c3ecf7c3ca836051d1ca03535c52d8d1e5
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-04-13 16:21:40 -0700

    wip FileSystemStore::GetPlayerScore(name) OK

--- http-server/http-server/file_system_store.go
+++ http-server/http-server/file_system_store.go
@@ -14,3 +14,14 @@ func (f *FileSystemStore) GetLeague() []Player {
 
 	return league
 }
+
+func (f *FileSystemStore) GetPlayerScore(name string) int {
+	var wins int
+	for _, player := range f.GetLeague() {
+		if player.Name == name {
+			wins = player.Wins
+			break
+		}
+	}
+	return wins
+}
