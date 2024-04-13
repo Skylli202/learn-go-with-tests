46fd5d8461af8a7677b5ba016344ccb645677297
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-04-13 16:16:51 -0700

    wip FileSystemStore::GetLeague() OK

--- http-server/http-server/file_system_store.go
+++ http-server/http-server/file_system_store.go
@@ -5,10 +5,11 @@ import (
 )
 
 type FileSystemStore struct {
-	Database io.Reader
+	Database io.ReadSeeker
 }
 
 func (f *FileSystemStore) GetLeague() []Player {
+	f.Database.Seek(0, io.SeekStart)
 	league, _ := NewLeague(f.Database)
 
 	return league
