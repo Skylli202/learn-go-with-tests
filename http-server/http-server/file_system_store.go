b136903db0ceedd23e2c86944cff120c9e4f0e38
Elouan GOUINGUENET <elouangouinguenet@yahoo.fr>
2024-04-14 12:19:46 -0700

    refactor player db constructor

--- http-server/http-server/file_system_store.go
+++ http-server/http-server/file_system_store.go
@@ -12,12 +12,12 @@ type FileSystemPlayerStore struct {
 	League   League
 }
 
-func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+func initializePlayerDBFile(file *os.File) error {
 	file.Seek(0, io.SeekStart)
 
 	info, err := file.Stat()
 	if err != nil {
-		return nil, fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
+		return fmt.Errorf("problem getting file info from file %s, %v", file.Name(), err)
 	}
 
 	if info.Size() == 0 {
@@ -25,6 +25,14 @@ func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
 		file.Seek(0, io.SeekStart)
 	}
 
+	return nil
+}
+
+func NewFileSystemPlayerStore(file *os.File) (*FileSystemPlayerStore, error) {
+	err := initializePlayerDBFile(file)
+	if err != nil {
+		return nil, fmt.Errorf("problem initialising player db file, %v", err)
+	}
 	league, err := NewLeague(file)
 	if err != nil {
 		return nil, fmt.Errorf("problem loading player store from file %s, %v", file.Name(), err)
